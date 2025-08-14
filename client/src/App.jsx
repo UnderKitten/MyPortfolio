f9a7f61df1065ace36c824a57b79940bdaa1bad6
Maxim Churzin <mchurzin@outlook.com>
2025-08-14 13:44:18 -0400

    Add more routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './Pages/Home'
-import { NotFound } from './Pages/NotFound'
+import  Home  from './Pages/Home'
+import  Projects  from './Pages/Projects'
+import  About  from './Pages/About'
+import  Resume  from './Pages/Resume'
+import  NotFound  from './Pages/NotFound'
 
 const App = () => {
   return (
@@ -10,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<NotFound />}/>
