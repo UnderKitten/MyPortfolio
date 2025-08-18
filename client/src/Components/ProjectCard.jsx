9095ed2ce439dd621b0a8b35ffcb84bd8495040d
Maxim Churzin <mchurzin@outlook.com>
2025-08-18 16:29:19 -0400

    Quick edit font size

--- client/src/Components/ProjectCard.jsx
+++ client/src/Components/ProjectCard.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 const ProjectCard = (props) => {
   return (
-    <div className="relative mx-auto w-full max-w-md h-[500px] rounded-xl bg-white/5 border border-fuchsia-700/40 shadow-lg backdrop-blur-xl p-6 flex flex-col items-center space-y-4">
+    <div className="relative mx-auto w-full max-w-md sm:h-[500px] h-[450px] rounded-xl bg-white/5 border border-fuchsia-700/40 shadow-lg backdrop-blur-xl p-6 flex flex-col items-center space-y-4">
       <img
         src={props.image}
         alt={props.alt}
         className="w-44 h-44 mx-auto mb-4 rounded-lg shadow-md border border-fuchsia-400/30 object-cover"
       />
 
-      <h2 className="text-3xl font-bold text-fuchsia-400 mb-2 text-center">{props.title}</h2>
-      <h3 className="text-3xl font-bold text-fuchsia-400 mb-2 text-center">{props.technologies}</h3>
+      <h2 className="text-xl sm:text-3xl font-bold text-fuchsia-400 mb-2 text-center">{props.title}</h2>
+      <h3 className="text-lg sm:text-3xl font-bold text-fuchsia-400 mb-2 text-center">{props.technologies}</h3>
 
-      <p className="text-white/90 text-xl mb-4 text-center">
+      <p className="text-base sm:text-xl text-white/90 mb-4 text-center">
         {props.shortDescription}
       </p>
       <div className="flex gap-10">
