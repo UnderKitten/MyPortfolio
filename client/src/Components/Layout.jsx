1404bb4779621c19acbd7680713f4b8b45f24dc1
Maxim Churzin <mchurzin@outlook.com>
2025-08-18 12:05:44 -0400

    Edit padding for smaller screen

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => (
   <div className="min-h-screen flex flex-col text-white">
   <Navbar />
 
-  <main className="relative flex-1 pt-30 pb-10 px-10 bg-gray-900 overflow-hidden">
+  <main className="relative flex-1 pt-30 pb-10 px-4 sm:px-10 bg-gray-900 overflow-hidden">
     <div className="absolute inset-0 bg-[linear-gradient(to_right,rgba(255,255,255,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.08)_1px,transparent_1px)] bg-[size:40px_40px] animate-grid"></div>
 
     <div className="absolute inset-0 bg-gradient-to-b from-transparent via-fuchsia-500/10 to-transparent"></div>
